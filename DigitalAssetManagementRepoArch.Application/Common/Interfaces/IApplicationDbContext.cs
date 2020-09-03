fd1df4feb40c9e00d63d79beb740b89f6f50ddff
Simon Cropper <simon.p.cropper@gmail.com>
2020-09-03 19:37:11 +0100

    Update commit

---
+++ DigitalAssetManagementRepoArch.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DigitalAssetManagementRepoArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DigitalAssetManagementRepoArch.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Phase> Phases { get; set; }
+        public DbSet<Strategy> Strategies { get; set; }
+        public DbSet<PhaseStrategy> PhaseStrategies { get; set; }
+        public DbSet<Ad> Ads { get; set; }
+        public DbSet<Channel> Channels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<PSChannel> PSChannels { get; set; }
+        public DbSet<PSChannelCountry> PSChannelCountries { get; set; }
+        public DbSet<PSChannelCountryAd> PSChannelCountryAds { get; set; }
+    }
+}
